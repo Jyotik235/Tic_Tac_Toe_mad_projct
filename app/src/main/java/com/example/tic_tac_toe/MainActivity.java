f4dd5985508c743a6012d65010c8334bc98c3cc5
Jyotik235 <201b342@juetguna.in>
2022-06-03 20:11:47 +0530

    changed font

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -67,7 +67,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-                
+                   /* new AlertDialog.Builder(this)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setMessage("Winner is Player " + (p == 0 ? 1 : 2))
+                            .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    Toast.makeText(getApplicationContext(), "congratulations", Toast.LENGTH_SHORT).show();
+                                }
+                            }).show();*/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
